7200f6148186ab34dae70b7494016216e3009fc9
Tom Chestnut <tchestnut85@gmail.com>
2021-05-14 22:44:19 -0400

    Created User model.

---
+++ server/models/User.js
@@ -0,0 +1,61 @@
+const { Schema, model } = mongoose;
+const bcrypt = require('bcrypt');
+
+const userSchema = new Schema(
+	{
+		username: {
+			type: String,
+			required: true,
+			trim: true,
+			unique: true,
+		},
+		name: {
+			type: String,
+			required: true,
+			trim: true,
+			minLength: 6,
+		},
+		email: {
+			type: String,
+			required: true,
+			trim: true,
+			unique: true,
+			match: [/.+@.+\..+/, 'Enter a valid email address.'],
+		},
+		password: {
+			type: String,
+			required: true,
+			trim: true,
+			minLength: 6,
+		},
+		teams: [
+			{
+				type: Schema.Types.ObjectId,
+				ref: 'Team',
+			},
+		],
+	},
+	{
+		toJSON: {
+			virtuals: true,
+			getters: true,
+		},
+	}
+);
+
+userSchema.pre('save', async function (next) {
+	if (this.isNew || this.isModified('password')) {
+		const saltRounds = 10;
+		this.password = await bcrypt.hash(this.password, saltRounds);
+	}
+
+	next();
+});
+
+userSchema.methods.isCorrectPassword = async function (password) {
+	return bcrypt.compare(password, this.password);
+};
+
+const User = model('User', userSchema);
+
+module.exports = User;
