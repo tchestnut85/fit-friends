9f92e05d22e18ba06aa4112950db056c4390a1db
Tom Chestnut <tchestnut85@gmail.com>
2021-05-16 17:25:53 -0400

    Added a route to edit/update a team.

--- server/routes/api/team-routes.js
+++ server/routes/api/team-routes.js
@@ -50,6 +50,26 @@ router.get('/', auth, async ({ body }, res) => {
 
 // Add user to a team
 // Put route - private
+router.put('/:id', auth, async ({ user, body, params }, res) => {
+    console.log('user:', user);
+    console.log('params:', params);
+    console.log('body:', body);
+
+    try {
+        const updatedTeam = await Team.findOneAndUpdate(
+            { _id: params.id },
+            body,
+            { new: true, runValidators: true }
+        );
+
+        res.json(updatedTeam);
+    } catch (err) {
+        console.error(`Error: ${err.message}`);
+        res.status(500).send({
+            message: `Server error: ${err.message}`,
+        });
+    }
+});
 
 // Remove user from a team
 // Put route - private
