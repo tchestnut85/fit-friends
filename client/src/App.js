938cd4bf513cdd9d5ce31e439f288d312c0606bc
Tom Chestnut <tchestnut85@gmail.com>
2021-05-22 14:03:58 -0400

    Created navbar and setup routing.

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,23 @@
+import './index.css';
+
+import { BrowserRouter as Router, Switch } from 'react-router-dom';
+
+import NavBar from './components/NavBar';
 import React from 'react';
+import { UserProvider } from './utils/state/UserState/UserState';
 
 function App() {
     return (
-        <div>
-            <h1>FIT FRIENDS!</h1>
-        </div>
+        <UserProvider>
+            <Router>
+                <NavBar />
+                <Switch>
+                    <div>
+                        <h1>FIT FRIENDS!</h1>
+                    </div>
+                </Switch>
+            </Router>
+        </UserProvider>
     );
 }
 
---
+++ client/src/components/NavBar/index.js
@@ -0,0 +1,32 @@
+import { Link } from 'react-router-dom';
+import React from 'react';
+import runIcon from '../../images/run_circle_black.svg';
+
+const NavBar = () => {
+    return (
+        <header className='nav-header px-1 py-1'>
+            <div className='centered'>
+                <img
+                    src={runIcon}
+                    alt='Icon of a runner'
+                    className='mx-1 run-icon'
+                />
+                <h1>
+                    <Link to='/'>FitFriends</Link>
+                </h1>
+            </div>
+            <nav>
+                <ul className='space-around'>
+                    <li>
+                        <Link to='/joinchallenge'>Join a Challenge</Link>
+                    </li>
+                    <li>
+                        <Link to='/createchallenge'>Create a Challenge</Link>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    );
+};
+
+export default NavBar;
