0aecf8044c326d261c3367812eb54847d2600165
Tom Chestnut <tchestnut85@gmail.com>
2021-06-15 21:54:44 -0400

    Updated Homepage to have a grid display with the logged in users info and teams they belong to or are the captain of. Used Semantic-UI for the cards and CSS grid for the layout, as well as added a HomepageCard component to reuse cards.

--- client/src/components/Loading/index.js
+++ client/src/components/Loading/index.js
@@ -2,5 +2,9 @@ import { Loader } from 'semantic-ui-react';
 import React from 'react';
 
 export const Loading = () => {
-    return <Loader active inline='centered' content='Loading...' />;
+    return (
+        <div className='my-3'>
+            <Loader active inline='centered' />;
+        </div>
+    );
 };
